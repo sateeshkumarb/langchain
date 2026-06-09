b67eab5e4b71e1fd24652a9560ec35c141e6f66e
Nick Hollon <nick.hollon@langchain.dev>
2026-06-09 19:51:26 -0400

    feat(openai): native content-block streaming for chat completions

--- libs/partners/openai/langchain_openai/__init__.py
+++ libs/partners/openai/langchain_openai/__init__.py
@@ -2,6 +2,10 @@
 
 from langchain_openai.chat_models import AzureChatOpenAI, ChatOpenAI
 from langchain_openai.chat_models._client_utils import StreamChunkTimeoutError
+from langchain_openai.chat_models._stream_events import (
+    aconvert_openai_completions_stream,
+    convert_openai_completions_stream,
+)
 from langchain_openai.embeddings import AzureOpenAIEmbeddings, OpenAIEmbeddings
 from langchain_openai.llms import AzureOpenAI, OpenAI
 from langchain_openai.tools import custom_tool
@@ -14,5 +18,7 @@ __all__ = [
     "OpenAI",
     "OpenAIEmbeddings",
     "StreamChunkTimeoutError",
+    "aconvert_openai_completions_stream",
+    "convert_openai_completions_stream",
     "custom_tool",
 ]
