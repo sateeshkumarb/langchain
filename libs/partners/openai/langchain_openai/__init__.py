f5ca4193e8bb899c5aa3e6e57aa97c0080ecef9e
Nick Hollon <nick.hollon@langchain.dev>
2026-06-09 21:15:47 -0400

    feat(openai): native content-block streaming for the Responses API

--- libs/partners/openai/langchain_openai/__init__.py
+++ libs/partners/openai/langchain_openai/__init__.py
@@ -4,7 +4,9 @@ from langchain_openai.chat_models import AzureChatOpenAI, ChatOpenAI
 from langchain_openai.chat_models._client_utils import StreamChunkTimeoutError
 from langchain_openai.chat_models._stream_events import (
     aconvert_openai_completions_stream,
+    aconvert_openai_responses_stream,
     convert_openai_completions_stream,
+    convert_openai_responses_stream,
 )
 from langchain_openai.embeddings import AzureOpenAIEmbeddings, OpenAIEmbeddings
 from langchain_openai.llms import AzureOpenAI, OpenAI
@@ -19,6 +21,8 @@ __all__ = [
     "OpenAIEmbeddings",
     "StreamChunkTimeoutError",
     "aconvert_openai_completions_stream",
+    "aconvert_openai_responses_stream",
     "convert_openai_completions_stream",
+    "convert_openai_responses_stream",
     "custom_tool",
 ]
